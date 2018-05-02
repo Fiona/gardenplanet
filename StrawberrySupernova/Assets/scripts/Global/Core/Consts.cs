e41eff45b6e09fc99fd10b00b6c1511254b3e2eb
Fiona Summers <stompyblondie@gmail.com>
2018-05-02 22:00:16 +0100

    Renamed some garden planet thing

--- StrawberrySupernova/Assets/scripts/Global/Core/Consts.cs
+++ StrawberrySupernova/Assets/scripts/Global/Core/Consts.cs
@@ -15,7 +15,7 @@ namespace StrawberryNova
         // -----------------------------
 
         // Where to boot the app into
-        public const AppState INITIAL_APP_STATE = AppState.Game;
+        public const AppState INITIAL_APP_STATE = AppState.Logo;
 
         // -----------------------------
         // TILE DRAWING
