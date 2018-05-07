17069d732b2d99aad813494b793640c79e37854e
Fiona Summers <stompyblondie@gmail.com>
2018-05-07 09:50:55 +0100

    Updated some item visuals

--- GardenPlanet/Assets/scripts/Game/GUI/ItemHotbarNameplate.cs
+++ GardenPlanet/Assets/scripts/Game/GUI/ItemHotbarNameplate.cs
@@ -2,21 +2,27 @@
 using StompyBlondie;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GardenPlanet
 {
     public class ItemHotbarNameplate: MonoBehaviour
     {
         public TextMeshProUGUI text;
+        public UICopySize background;
 
         private CanvasGroup canvasGroup;
+        private bool show;
 
         public void Show(string itemName, Transform hotbarItem)
         {
-            if(Math.Abs(canvasGroup.alpha) < .05f)
-                StartCoroutine(LerpHelper.QuickFadeIn(canvasGroup, .5f, lerpType: LerpHelper.Type.SmoothStep));
-            canvasGroup.alpha = 1f;
+            if(!show)
+            {
+                StartCoroutine(LerpHelper.QuickFadeIn(canvasGroup, .25f, lerpType: LerpHelper.Type.SmoothStep));
+                show = true;
+            }
             text.text = itemName;
+            text.ForceMeshUpdate();
             transform.position = new Vector3(
                 hotbarItem.position.x,
                 transform.position.y,
@@ -26,14 +32,16 @@ namespace GardenPlanet
 
         public void Hide()
         {
-            if(Math.Abs(canvasGroup.alpha) > .95f)
-                StartCoroutine(LerpHelper.QuickFadeOut(canvasGroup, .5f, lerpType: LerpHelper.Type.SmoothStep));
-            canvasGroup.alpha = 0f;
+            if(show)
+                StartCoroutine(LerpHelper.QuickFadeOut(canvasGroup, .25f, lerpType: LerpHelper.Type.SmoothStep));
+            show = false;
         }
 
         private void Awake()
         {
             canvasGroup = GetComponent<CanvasGroup>();
+            canvasGroup.alpha = 0f;
+            show = false;
         }
 
     }
