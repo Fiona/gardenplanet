d30e52293bd8a5380c3a70d4f9a9e48bdb96f771
Fiona Summers <yum@fiona.pizza>
2018-08-06 14:07:50 +0100

    Finished inventory tests and made them pass

--- GardenPlanet/Assets/scripts/Game/Inventory.cs
+++ GardenPlanet/Assets/scripts/Game/Inventory.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using LitJson;
 using UnityEngine;
@@ -55,79 +56,20 @@ namespace GardenPlanet
          */
         public bool AddItem(ItemType itemType, Attributes attributes, int quantity = 1)
         {
-
-            // Check if we have any stacks of this type
-            var existingEntries = GetItemEntriesOfType(itemType, attributes);
-
-            // If we don't have any add a new one
-            if(existingEntries.Count == 0)
-            {
-                // Check we haven't accidently got to the inventory limit
-                if(items.Count >= maximumItemStacks)
-                    return false;
-                // Okay to start a new stack
-                items.Add(
-                    new InventoryItemEntry {
-                        itemType = itemType,
-                        quantity = quantity,
-                        attributes = new Attributes(attributes)
-                    }
-                );
-                return true;
-            }
-
-            // We have existing entries, let's see if we can fit in one of them
-            foreach(var entry in existingEntries)
-            {
-                // If we can fit in this stack then use it
-                if(entry.quantity + quantity <= itemType.StackSize)
-                {
-                    entry.quantity += quantity;
-                    return true;
-                }
-            }
-
-            // If we got here then none of the existing stacks were appropriate
-            // so we check if we have room for a new stack afterall.
-            if(items.Count < maximumItemStacks)
-            {
-                items.Add(
-                    new InventoryItemEntry
-                    {
-                        itemType = itemType,
-                        quantity = quantity,
-                        attributes = new Attributes(attributes)
-                    }
-                );
-                return true;
-            }
-
-            // The item doesn't fit into the inventory at all
-            return false;
-
+            if(!ItemAddSuccessProbe(itemType, attributes, quantity))
+                return false;
+            return DoItemAdd(itemType, attributes, quantity, items);
         }
 
         /*
-         * Removes one or more items of a passed type from the inventory. Will always
-         * attempt to remove up to the quantity given.
-         * Return false if the item wasn't there or true otherwise.
+         * Removes one or more items of a passed type from the inventory.
+         * Return false if there weren't enough of the item in the inventory or true otherwise.
          */
         public bool RemoveItem(ItemType itemType, Attributes attributes, int quantity = 1)
         {
-            var entries = GetItemEntriesOfType(itemType, attributes);
-            // We don't have that type in the inventory
-            if(entries.Count == 0)
+            if(!ItemRemoveSuccessProbe(itemType, attributes, quantity))
                 return false;
-
-            // Reduce by amount
-            entries[0].quantity -= quantity;
-
-            // Destroy any entries that no longer have an item in
-            var cloneList = new List<InventoryItemEntry>(items);
-            foreach(var entry in cloneList)
-                if(entry.quantity <= 0)
-                    items.Remove(entry);
-            return true;
+            return DoItemRemove(itemType, attributes, quantity, items);
         }
 
         /*
@@ -146,20 +88,109 @@ namespace GardenPlanet
             return true;
         }
 
-        List<InventoryItemEntry> GetItemEntriesOfType(ItemType itemType, Attributes attributes)
-        {
-            var list = new List<InventoryItemEntry>();
-            foreach(var item in items)
-            {
-                if(item.itemType == itemType && attributes == item.attributes)
-                    list.Add(item);
-            }
-            return list;
-        }
-
         public bool ItemEntryExists(InventoryItemEntry entry)
         {
             return items.IndexOf(entry) > -1;
+        }
+
+        List<InventoryItemEntry> GetItemEntriesOfType(ItemType itemType, Attributes attributes,
+            List<InventoryItemEntry> itemList = null)
+        {
+            if(itemList == null)
+                itemList = items;
+            var list = new List<InventoryItemEntry>();
+            foreach(var item in itemList)
+                if(item.itemType == itemType && attributes == item.attributes)
+                    list.Add(item);
+            return list;
+        }
+
+        bool ItemAddSuccessProbe(ItemType itemType, Attributes attributes, int quantity)
+        {
+            return DoItemAdd(itemType, attributes, quantity, CloneItemList(items));
+        }
+
+        bool DoItemAdd(ItemType itemType, Attributes attributes, int quantity, List<InventoryItemEntry> itemList)
+        {
+            // We managed to add everything
+            if(quantity <= 0)
+                return true;
+
+            // Try and get existing stacks with room, we try to fill those first
+            var existingStacks = GetItemEntriesOfType(itemType, attributes, itemList)
+                .Where(x => x.quantity < itemType.StackSize).ToList();
+
+            if(existingStacks.Count > 0)
+            {
+                var spaceRemaining = itemType.StackSize - existingStacks[0].quantity;
+
+                // Only add up to the remaining space or whole quantity depending on how much
+                // is left
+                var quantityToAdd = quantity <= spaceRemaining ? quantity : spaceRemaining;
+                existingStacks[0].quantity += quantityToAdd;
+
+                // Go round again fitting the rest in
+                return DoItemAdd(itemType, attributes, quantity - quantityToAdd, itemList);
+            }
+
+            // If there's room for another stack then add it
+            if(maximumItemStacks > itemList.Count)
+            {
+                var quantityToAdd = quantity < itemType.StackSize ? quantity : itemType.StackSize;
+                itemList.Add(new InventoryItemEntry{
+                    itemType = itemType,
+                    attributes = new Attributes(attributes),
+                    quantity = quantityToAdd
+                });
+                // Recurse, trying to fit remaining in
+                return DoItemAdd(itemType, attributes, quantity - quantityToAdd, itemList);
+            }
+
+            return false;
+        }
+
+        bool ItemRemoveSuccessProbe(ItemType itemType, Attributes attributes, int quantity)
+        {
+            return DoItemRemove(itemType, attributes, quantity, CloneItemList(items));
+        }
+
+        bool DoItemRemove(ItemType itemType, Attributes attributes, int quantity, List<InventoryItemEntry> itemList)
+        {
+            // We managed to remove everything
+            if(quantity <= 0)
+                return true;
+            // Try and get existing stacks, we try to take frome those first
+            var existingStacks = GetItemEntriesOfType(itemType, attributes, itemList)
+                .OrderBy(x => x.quantity).ToList();
+            // If none exist then we can't remove them
+            if(existingStacks.Count == 0)
+                return false;
+            // See how many we can take from the first stack
+            var quantityToRemove = quantity <= existingStacks[0].quantity ? quantity : existingStacks[0].quantity;
+            // Remove items
+            existingStacks[0].quantity -= quantityToRemove;
+            // Destroy entry if empty
+            if(existingStacks[0].quantity <= 0)
+                itemList.Remove(existingStacks[0]);
+            // Recurse, trying to remove the remaining quantity
+            return DoItemRemove(itemType, attributes, quantity - quantityToRemove, itemList);
+        }
+
+        List<InventoryItemEntry> CloneItemList(List<InventoryItemEntry> toClone)
+        {
+            var newItemList = new List<InventoryItemEntry>();
+            foreach(var curItem in items)
+            {
+                newItemList.Add(
+                    new InventoryItemEntry
+                    {
+                        itemType = curItem.itemType,
+                        quantity = curItem.quantity,
+                        attributes = new Attributes(curItem.attributes)
+                    }
+                );
+            }
+            return newItemList;
         }
 
     }
