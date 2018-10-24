f7a3a3793b1f087f1f3808c0aec34e0650fbfc2f
Fiona Summers <yum@fiona.pizza>
2018-10-24 17:13:06 +0100

    Implemented astar with tests

--- GardenPlanet/Assets/scripts/Global/Core/Consts.cs
+++ GardenPlanet/Assets/scripts/Global/Core/Consts.cs
@@ -15,7 +15,7 @@ namespace GardenPlanet
         // -----------------------------
 
         // Where to boot the app into
-        public const AppState INITIAL_APP_STATE = AppState.Game;
+        public const AppState INITIAL_APP_STATE = AppState.CreateACharacter;
 
         // What the first map name is
         public const string START_MAP = "devtest";
